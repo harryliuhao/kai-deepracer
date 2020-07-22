4c351c2717b5dc57fbe041f694e083e030a124ac
Kaichen Liu <harryliuhao@hotmail.com>
2020-07-22 09:17:14 -0400

    updated reward function

--- turtle-1.py
+++ turtle-1.py
@@ -2,20 +2,20 @@ def reward_function(params):
     '''
     Example of rewarding the agent to follow center line
     '''
-    
+
     # Read input parameters
     track_width = params['track_width']
     distance_from_center = params['distance_from_center']
     all_wheels_on_track = params['all_wheels_on_track']
     speed = params['speed']
-    SPEED_THRESHOLD = 1.0 
-    steering = abs(params['steering_angle']) 
-    
+    SPEED_THRESHOLD = 1.0
+    steering = abs(params['steering_angle'])
+
     # Calculate 3 markers that are at varying distances away from the center line
     marker_1 = 0.1 * track_width
     marker_2 = 0.25 * track_width
     marker_3 = 0.5 * track_width
-    
+
     # Give higher reward if the car is closer to center line and vice versa
     if distance_from_center <= marker_1:
         reward = 1.0
@@ -23,20 +23,13 @@ def reward_function(params):
         reward = 0.5
     elif distance_from_center <= marker_3:
         reward = 0.1
+    elif not all_wheels_on_track:
+        reward = 1e-3
+    elif speed < SPEED_THRESHOLD:
+        reward *= 0.5
     else:
         reward = 1e-3  # likely crashed/ close to off track
-        
-    if not all_wheels_on_track:		
-        reward = 1e-3  # Penalize if the car goes off track
-    elif speed < SPEED_THRESHOLD:		
-        reward *= 0.5  # Penalize if the car goes too slow
-    else:
-        # High reward if the car stays on track and goes fast
-        reward = 1.0
-    
-    #STEERING_THRESHOLD = 20.0
-    #if steering > STEERING_THRESHOLD:
-    #    reward *= 0.8
-        
+
+
+
     return float(reward)
-        
