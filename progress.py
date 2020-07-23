ee60df1e62b2dac5b8aa16c16914d3c727d2d827
Hao Liu <harryliuhao@hotmail.com>
2020-07-23 11:31:23 -0400

    updated progress function

--- progress.py
+++ progress.py
@@ -1,6 +1,15 @@
 def reward_function(params):
-    if not params["all_wheels_on_track"]:
+    SPEED_THRESHOLD = 1.0
+    all_wheels_on_track = params['all_wheels_on_track']
+    speed = params['speed']
+    completion=params["progress"]
+
+    if not all_wheels_on_track:
         reward = -1
     else:
-        reward = params["progress"]
+        reward = completion
+
+    if speed < SPEED_THRESHOLD and all_wheels_on_track:
+        reward *= 0.5  # Penalize if the car goes too slow
+
     return reward
